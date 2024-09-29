f58c71174d3d9b9c761985625bfa449be829a653
MATRIX-feather <midnightcarnival@outlook.com>
2024-09-29 16:30:25 +0800

    add: 用于打开设置的快捷键

--- src/main/java/xyz/nifeather/lingstweaks/client/TweakModClient.java
+++ src/main/java/xyz/nifeather/lingstweaks/client/TweakModClient.java
@@ -1,13 +1,18 @@
 package xyz.nifeather.lingstweaks.client;
 
+import com.mojang.blaze3d.platform.InputConstants;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
+import org.lwjgl.glfw.GLFW;
+import xyz.nifeather.lingstweaks.config.ConfigScreenProvider;
 
 @Environment(EnvType.CLIENT)
 public class TweakModClient implements ClientModInitializer
@@ -15,6 +20,8 @@ public class TweakModClient implements ClientModInitializer
     public static boolean enabled;
 
     private TweakClient tweakClient;
+
+    private KeyMapping keyOpenSettings;
 
     /**
      * Runs the mod initializer on the client environment.
@@ -27,10 +34,17 @@ public class TweakModClient implements ClientModInitializer
         this.tweakClient = TweakClient.instance();
         tweakClient.modInit();
 
-        ClientTickEvents.START_CLIENT_TICK.register(this::tick);
+        ClientTickEvents.END_CLIENT_TICK.register(this::tick);
+
+        keyOpenSettings = KeyBindingHelper.registerKeyBinding(new KeyMapping(
+                "key.lingsTweaks.openSettings", InputConstants.Type.KEYSYM,
+                GLFW.GLFW_KEY_UNKNOWN, "category.lingsTweaks.keybind"
+        ));
     }
 
     private void tick(Minecraft minecraft)
     {
+        if (keyOpenSettings.consumeClick())
+            minecraft.setScreen(ConfigScreenProvider.createScreen(tweakClient, null));
     }
 }
